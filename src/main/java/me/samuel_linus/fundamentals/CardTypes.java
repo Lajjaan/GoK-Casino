044eb1fa5cbed91f98717cd1ff841d558db48942
Linus Bäckberg <81620730+Lajjaan@users.noreply.github.com>
2023-09-11 21:14:53 +0200

    The deck was created

---
+++ src/main/java/me/samuel_linus/fundamentals/CardTypes.java
@@ -0,0 +1,20 @@
+package me.samuel_linus.fundamentals;
+
+public enum CardTypes {
+
+    Ess,
+    King,
+    Queen,
+    Cracked,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two;
+
+}
+
--- src/main/java/me/samuel_linus/fundamentals/Deck.java
+++ src/main/java/me/samuel_linus/fundamentals/Deck.java
@@ -1,4 +1,31 @@
 package me.samuel_linus.fundamentals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Deck {
+
+    Map<CardTypes, Integer> cardValues = new HashMap<>();
+
+    public Deck() {
+
+        cardValues.put(CardTypes.King, 10);
+        cardValues.put(CardTypes.Queen, 10);
+        cardValues.put(CardTypes.Cracked, 10);
+        cardValues.put(CardTypes.Ten, 10);
+        cardValues.put(CardTypes.Nine, 9);
+        cardValues.put(CardTypes.Eight, 8);
+        cardValues.put(CardTypes.Seven, 7);
+        cardValues.put(CardTypes.Six, 6);
+        cardValues.put(CardTypes.Five, 5);
+        cardValues.put(CardTypes.Four, 4);
+        cardValues.put(CardTypes.Three, 3);
+        cardValues.put(CardTypes.Two, 2);
+
+    }
+
+    public Integer getCardValue(CardTypes cardType) {
+        return cardValues.get(cardType);
+    }
+
 }
